42d307850173d5ee65ffa3e73293fc4c90aac120
robert neuzil <robertneuzil@gmail.com>
2018-10-26 21:27:53 -0400

    all

--- pollme/urls.py
+++ pollme/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.current_datetime)
+    path('time/', views.current_datetime),
+    path('home/', views.home),
 ]
--- pollme/views.py
+++ pollme/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
     html = f'<h1>The current time is {now}</h1>'
     return HttpResponse(html)
+
+def home(request):
+	return render(request, 'home.html')
