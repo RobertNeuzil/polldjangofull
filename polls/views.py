2a062968ddeffe6b8cfdf5fdc73588128813695d
robert neuzil <robertneuzil@gmail.com>
2018-10-27 00:13:03 -0400

    urlzzzz

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+
 
 def polls_list(request):
-	return HttpResponse("Polls List")
+	now = datetime.datetime.now()
+	context = { "GOT" : now }
+	return render(request, 'polls/pollslist.html', context)
 
 # Create your views here.
